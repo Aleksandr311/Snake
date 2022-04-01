26418f9323ba5776a858c052063154578a860746
Aleks <chernovalex311.gmail.com>
2022-04-01 20:27:22 +0700

    пытаемся создать змеюку

--- Game/const.js
+++ Game/const.js
@@ -1,3 +1,4 @@
+//map
 const MAP_WIDTH = 20
 const MAP_HEIGHT = 20
 const MAP_POINT_SIZE = 16
@@ -6,4 +7,26 @@ export {
     MAP_WIDTH,
     MAP_HEIGHT,
     MAP_POINT_SIZE,
+}
+
+//snake
+
+const SNAKE_POSITION_HEAD_X = 10
+const SNAKE_POSITION_HEAD_Y = 10
+const SNAKE_POSITION_BODY_X = [9, 8]
+const SNAKE_POSITION_BODY_Y = [10, 10]
+const SNAKE_LENGTH = 3 //ЯЧЕЙКИ
+const SNAKE_DIRECTION = "right"//"up","down","left" возможные значения
+const SNAKE_SPEED = 0
+
+
+export {
+    
+    SNAKE_POSITION_HEAD_X,
+    SNAKE_POSITION_HEAD_Y,
+    SNAKE_POSITION_BODY_X,
+    SNAKE_POSITION_BODY_Y,
+    SNAKE_LENGTH,
+    SNAKE_DIRECTION,
+    SNAKE_SPEED,
 }
--- Game/index.js
+++ Game/index.js
@@ -1,8 +1,10 @@
-import {MAP_WIDTH, MAP_HEIGHT, MAP_POINT_SIZE,} from "./const.js"
+import {MAP_WIDTH, MAP_HEIGHT, MAP_POINT_SIZE, SPEED,} from "./const.js"
 import Snake from "../Core/Snake.js";
 import Map from "../Core/Map.js";
+import {SNAKE_LENGTH, DIRECTION, SPEED, SNAKE_POSITION_HEAD_X, SNAKE_POSITION_HEAD_Y}
 
-const game = {
+
+from game = {
     map: {},
     snake: {},
     apple: {},
@@ -12,8 +14,18 @@ const game = {
         Map.setHeight(MAP_HEIGHT);
         Map.setPointSize(MAP_POINT_SIZE);
         this.map = Map;
-        
     },
+    init(){        
+        Snake.setPositionHead(SNAKE_POSITION_HEAD_X);
+        Snake.setPositionHead(SNAKE_POSITION_HEAD_Y);
+        Snake.setPositionBody(SNAKE_POSITION_BODY_X);
+        Snake.setPositionBody(SNAKE_POSITION_BODY_Y);
+        Snake.setLength(SNAKE_LENGTH);
+        Snake.setDirection(DIRECTION);
+        Snake.setSpeed(SPEED);
+
+        this.snake = Snake;
+        },
     spawnSnake(){},
     spawnApple(){},
     start(){},
